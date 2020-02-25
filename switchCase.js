6cb3f1c0579330ddaf667561e99a3557ea0aa7de
Samirudin242 <61079102+Samirudin242@users.noreply.github.com>
2020-02-25 14:08:19 +0700

    added-file

---
+++ switchCase.js
@@ -0,0 +1,80 @@
+var hari = 29;
+var bulan = 6;
+var tahun = 1997;
+
+
+switch (bulan) {
+    case 1:
+    bulan = 'Januari';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 2:
+    bulan = 'Februari';
+    if (hari >=1 && hari <= 31 && tahun >=1900  &&  tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 3:
+    bulan = 'Maret';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break;
+    case 4:
+    bulan = 'April';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break;  
+    case 5:
+    bulan = 'Mei';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 6:
+    bulan = 'Juni';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 7:
+    bulan = 'Juli';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 8:
+    bulan = 'Agustus';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 9:
+    bulan = 'September';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 10:
+    bulan = 'Oktober';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 11:
+    bulan = 'November';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+    case 12:
+    bulan = 'Desember';
+    if (hari >=1 && hari <= 31 && tahun >=1900  && tahun <= 2200) {
+        console.log(hari +  ' ' + bulan + ' ' + tahun);
+    }
+    break; 
+}
+
